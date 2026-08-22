91d00130adda453a78a8c929ea3b698d521852dd
hashim049 <MHASHIM92111@GMAIL.COM>
2026-08-22 20:17:50 +0530

    Add Supabase client configuration

---
+++ admin/config.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://atguvpoxjzutvpyeqzai.supabase.co";
+
+const SUPABASE_KEY =
+  "sb_publishable_bloeT0C8P9HFvLugiE7lnA_4lwQffUk";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
