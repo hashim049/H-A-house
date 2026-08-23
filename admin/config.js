29ab726d6d734ef2972d2146e64c2e0cc0574b49
hashim049 <MHASHIM92111@GMAIL.COM>
2026-08-23 17:43:03 +0530

    Update config.js

--- admin/config.js
+++ admin/config.js
@@ -1,9 +1,11 @@
-const SUPABASE_URL = "https://atguvpoxjzutvpyeqzai.supabase.co";
+const SUPABASE_URL =
+  "https://atguvpoxjzutvpyeqzai.supabase.co";
 
 const SUPABASE_KEY =
   "sb_publishable_bloeT0C8P9HFvLugiE7lnA_4lwQffUk";
 
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+const supabaseClient =
+  window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+  );
